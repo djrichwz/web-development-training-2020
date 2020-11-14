af41a1376fe6f9ce54f1dbc97cd23ec61a46c59b
Richard Wimbridge <richardwimbridge@hotmail.com>
2020-11-14 16:20:29 +0000

    Added click event for all buttons

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,3 +1,7 @@
-function handleClick(){
-  alert("I got clicked");
+
+var buttons = document.querySelectorAll("button");
+for(var i = 0; i < buttons.length; i++){
+  buttons[i].addEventListener("click", function(){
+    alert("I got clicked");
+  });
 }
