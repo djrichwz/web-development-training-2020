c60562d23129c8f73f03a04b515aca762c7a2e9f
Richard Wimbridge <richardwimbridge@hotmail.com>
2020-11-18 17:32:48 +0000

    Added in code to dismiss the alrt and fade it out and also added a way to do something before the alert Closes by dispatching closed.alert event

--- Website Elements Practice/js/alert.js
+++ Website Elements Practice/js/alert.js
@@ -1,7 +1,29 @@
+
+
+
 document.querySelector(".alert-dismissible .close").addEventListener("click", function(event){
   var dataToDismiss = this.getAttribute("data-dismiss");
   var parentElement = this.parentElement;
-    if(parentElement.classList.contains(dataToDismiss)){
-      parentElement.classList.remove("show");
-    }
+
+  document.dispatchEvent(new Event("closed.alert"));
+
+  if(parentElement.classList.contains(dataToDismiss)){
+    dismiss(parentElement);
+  }
 });
+
+
+
+function dismiss(element){
+  if(element.classList.contains("show") && element.classList.contains("fade")){
+    element.classList.remove("show");
+
+    setTimeout(function(){
+      element.remove();
+    }, 200)
+  }
+  else{
+    element.remove();
+  }
+
+}
---
+++ Website Elements Practice/js/index.js
@@ -0,0 +1,3 @@
+document.addEventListener("closed.alert", function(e){
+  console.log("alert has been closed do something here");
+}, false);
